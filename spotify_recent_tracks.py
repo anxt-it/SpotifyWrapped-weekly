5436803a66e0eb660b321f35f2450c3eb12466f6
anxt-it <anataorevi123@gmail.com>
2026-03-15 18:02:05 +0200

    Get recent tracks from Spotify

---
+++ spotify_recent_tracks.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+
+SCOPE = "user-read-recently-played"
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri=redirect_uri,
+    scope=SCOPE
+))
+
+
+def get_recent_tracks(sp):
+    return sp.current_user_recently_played(limit=40)
+
