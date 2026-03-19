88155e70941beca541593acee169e5570fd3a5a0
anxt-it <anataorevi123@gmail.com>
2026-03-19 18:44:45 +0200

    Wrap in function

--- collector.py
+++ collector.py
@@ -1,54 +1,51 @@
 import sqlite3
 
 # make the request
-from spotify_client import sp_results as raw_results
+from spotify_client import fetch_recent_tracks
 from recent_tracks import get_recent_tracks
 
-from clean_recent_tracks import load_df, clean_data_frame
+def run_pipeline():
+
+    # extraction
+    raw_results = fetch_recent_tracks(limit=3)
+
+    if "error" in raw_results:
+        raise RuntimeError(f"Spotify API error {raw_results["error"]["status"]}: {raw_results["error"]["message"]}")
+
+    records = get_recent_tracks(raw_results)
 
 
-# get extracted data
-if "error" in raw_results:
-    raise RuntimeError(f"Spotify API error {raw_results["error"]["status"]}: {raw_results["error"]["message"]}")
+    # load
+    data_to_insert = [
+        (r['track_id'], r['track_name'], r['artist_id'], r['artist_name'],
+         r['album_id'], r['album_name'], r['duration_ms'], r['played_at'])
+        for r in records
+    ]
 
-records = get_recent_tracks(raw_results)
+    with sqlite3.connect('spotify_test.db') as conn:
+        cursor = conn.cursor()
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS listening_history (
+            track_id TEXT,  
+            track_name TEXT,
+            artist_id TEXT,
+            artist_name TEXT,
+            album_id TEXT,
+            album_name TEXT,
+            duration_ms INT, 
+            played_at DATETIME,
+            UNIQUE (played_at)
 
-# clean the data
-df = load_df(records)
-cleaned_df = clean_data_frame(df)
+        );
+        """
 
-# write dataframe to sqlite
-TEST_FILE_DB = 'spotify_test.db'
+        cursor.execute(create_table_query)
 
-conn = sqlite3.connect(TEST_FILE_DB)
-cursor = conn.cursor()
+        cursor.executemany(
+            "INSERT OR IGNORE INTO listening_history VALUES (?,?,?,?,?,?,?,?)",
+            data_to_insert
+        )
+        print(f"Done. Check your DB, we just processed {len(data_to_insert)} tracks.")
 
-create_table_query = """
-CREATE TABLE IF NOT EXISTS listening_history (
-    track_id TEXT,  
-    track_name TEXT,
-    artist_id TEXT,
-    artist_name TEXT,
-    album_id TEXT,
-    album_name TEXT,
-    duration_ms INT, 
-    played_at DATETIME,
-    UNIQUE (played_at)
-    
-);
-"""
-
-cursor.execute(create_table_query) # execute the sql command
-
-
-insert_or_ignore_query = """
-    INSERT OR IGNORE INTO listening_history
-    (track_id, track_name, artist_id, artist_name, album_id, album_name, duration_ms, played_at)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-"""
-
-cursor.executemany(insert_or_ignore_query, cleaned_df)
-
-conn.commit() # commit the changes
-conn.close()
-
+if __name__ == "__main__":
+    run_pipeline()
