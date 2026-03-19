41beb1eaa0a1e20db5ba0d379f1f5ca8d5264681
anxt-it <anataorevi123@gmail.com>
2026-03-19 14:02:23 +0200

    Clean records data

--- clean_recent_tracks.py
+++ clean_recent_tracks.py
@@ -1,1 +1,17 @@
 
+import pandas as pd
+
+# load file and return DataFrame
+def load_df(data):
+    return pd.DataFrame(data)
+
+
+def clean_data_frame(df):
+    # dtypes
+    df['played_at'] = pd.to_datetime(df['played_at'])
+    return df
+
+
+# this is for test purposes only
+from recent_tracks import test_data
+# df = load_df(test_data)
