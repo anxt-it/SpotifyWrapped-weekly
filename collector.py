991c35cb1addc1823f9bc2baa944bf759771dbd7
anxt-it <anataorevi123@gmail.com>
2026-03-20 14:35:59 +0200

    Update extraction limit

--- collector.py
+++ collector.py
@@ -7,13 +7,12 @@ from recent_tracks import get_recent_tracks
 def run_pipeline():
 
     # extraction
-    raw_results = fetch_recent_tracks(limit=3)
+    raw_results = fetch_recent_tracks(limit=50)
 
     if "error" in raw_results:
         raise RuntimeError(f"Spotify API error {raw_results["error"]["status"]}: {raw_results["error"]["message"]}")
 
     records = get_recent_tracks(raw_results)
-
 
     # load
     data_to_insert = [
@@ -33,19 +32,22 @@ def run_pipeline():
             album_id TEXT,
             album_name TEXT,
             duration_ms INT, 
-            played_at DATETIME,
-            UNIQUE (played_at)
-
+            played_at DATETIME UNIQUE,
+            extracted_at DATETIME DEFAULT CURRENT_TIMESTAMP
         );
         """
 
         cursor.execute(create_table_query)
 
-        cursor.executemany(
-            "INSERT OR IGNORE INTO listening_history VALUES (?,?,?,?,?,?,?,?)",
-            data_to_insert
-        )
-        print(f"Done. Check your DB, we just processed {len(data_to_insert)} tracks.")
+        insert_or_ignore_query = """
+                    INSERT OR IGNORE INTO listening_history 
+                    (track_id, track_name, artist_id, artist_name, album_id, album_name, duration_ms, played_at) 
+                    VALUES (?,?,?,?,?,?,?,?)
+                    """
+
+        cursor.executemany(insert_or_ignore_query, data_to_insert)
+
+        print(f"Done. Check your DB, just processed {len(data_to_insert)} tracks.")
 
 if __name__ == "__main__":
     run_pipeline()
