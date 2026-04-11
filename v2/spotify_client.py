5222ba473bb893e6d20abb4d2e7935de1041e9c8
anxt-it <anataorevi123@gmail.com>
2026-04-11 16:36:45 +0300

    fixed v1 spotify_client

--- v2/spotify_client.py
+++ v2/spotify_client.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import spotipy
+from spotipy import SpotifyException
 from spotipy.oauth2 import SpotifyOAuth
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +13,6 @@ client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
@@ -22,4 +22,19 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 ))
 
 def fetch_recent_tracks(limit=50):
-    return sp.current_user_recently_played(limit=limit)
+
+    try:
+        data = sp.current_user_recently_played(limit=limit)
+        return {
+        "success": True,
+        "data": data,
+        "error": None
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "data": None,
+            "error": str(e)
+        }
+
+
